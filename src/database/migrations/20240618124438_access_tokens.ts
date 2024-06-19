19d93e8a832a7693f9acfda306d2a55e55a6c15b
iv <consmebius@gmail.com>
2024-06-19 13:35:51 +0300

    add refresh_tokens

---
+++ src/database/migrations/20240618124438_access_tokens.ts
@@ -0,0 +1,17 @@
+import { Client } from 'pg';
+
+export const up = async (client: Client) => {
+  await client.query(`
+    CREATE TABLE access_tokens (
+    token_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+};
+
+export const down = async (client: Client) => {
+  await client.query('DROP TABLE access_tokens');
+};
