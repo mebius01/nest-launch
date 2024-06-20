5bc3df2dfe144e1350f6bffa1f4fdfe358f647de
iv <consmebius@gmail.com>
2024-06-20 09:42:50 +0300

    add roles

---
+++ src/database/migrations/20240619211158_roles.ts
@@ -0,0 +1,16 @@
+import { Client } from 'pg';
+
+const TABLE_NAME = 'roles';
+
+export const up = async (client: Client) => {
+  await client.query(`
+    CREATE TABLE ${TABLE_NAME} (
+      role_id SERIAL PRIMARY KEY,
+      role_name VARCHAR(255) NOT NULL
+    );
+  `);
+};
+
+export const down = async (client: Client) => {
+  await client.query(`DROP TABLE ${TABLE_NAME}`);
+};
